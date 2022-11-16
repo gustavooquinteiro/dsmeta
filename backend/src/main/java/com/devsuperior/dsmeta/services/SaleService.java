04a51ebde876149916f5fec7999cadfd9d9edb2f
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-16 14:00:47 -0300

    add custom default rule to SaleService

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,8 +18,11 @@ public class SaleService {
 	
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
 		
-		LocalDate min = LocalDate.parse(minDate);
-		LocalDate max = LocalDate.parse(maxDate);
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate oneYearAgo = today.minusYears(1);
+		
+		LocalDate min = (minDate.equals(""))? oneYearAgo : LocalDate.parse(minDate);
+		LocalDate max = (maxDate.equals("")) ? today : LocalDate.parse(maxDate);
 		
 		return repository.findAll(min, max, pageable);
 	}
