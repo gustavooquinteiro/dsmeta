ac6f90ed5712e7efddae619203376c28225d5f42
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-16 13:35:11 -0300

    create findSales method

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,5 +1,20 @@
 package com.devsuperior.dsmeta.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+
 public class SaleService {
 
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales(){
+		return repository.findAll();
+	}
+	
+	
 }
