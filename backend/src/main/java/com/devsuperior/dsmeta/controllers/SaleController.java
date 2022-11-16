d7ecb55a70a3542347b17cf1a28f0002ad9d3ba4
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-16 13:45:15 -0300

    add pageable

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,8 +1,8 @@
 package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class SaleController {
 	private SaleService service;
 
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(Pageable pageable) {
+		return service.findSales(pageable);
 	}
 }
